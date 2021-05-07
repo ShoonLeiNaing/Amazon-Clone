dffa8bd01ac64ff5cf68d22fa1ecbbf76453985e
Shoon Lei <63739542+ShoonLeiNaing@users.noreply.github.com>
2021-05-07 22:19:18 +0630

    nav done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+    {/* Header */}
+    <Header/>
+    {/* Home */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDhx9RTM_8Nrm6uAPUvvxhqP3SmMF7c9qU",
+    authDomain: "clone-4b31a.firebaseapp.com",
+    projectId: "clone-4b31a",
+    storageBucket: "clone-4b31a.appspot.com",
+    messagingSenderId: "773051492126",
+    appId: "1:773051492126:web:e479d21584478974d88b0f",
+    measurementId: "G-Y1X2PRWLSD"
+  };
